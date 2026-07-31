bc622052a4e2f654805641e2ce6f6eb1b7aceb5f
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-07-31 17:20:05 -0300

    feat: adiciona o cookie na main

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
+import cookieParser from 'cookie-parser';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
 
   app.useGlobalPipes(
     new ValidationPipe({
