2cb558595829f989f2ba89bc3094859c7b7d7439
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-07-31 17:28:14 -0300

    feat: adiciona o jwt strategy no módulo de auth

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -6,6 +6,7 @@ import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
@@ -25,6 +26,6 @@ import { AuthService } from './auth.service';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
