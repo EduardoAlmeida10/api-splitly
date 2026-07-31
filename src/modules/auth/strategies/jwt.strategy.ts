60ebb91b80042417423a9f3bf7044284b392d3d1
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-07-31 17:27:41 -0300

    feat: adiciona strategy de autenticação JWT

---
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import type { Request } from 'express';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+interface JwtPayload {
+  sub: string;
+  email: string;
+}
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => {
+          return request?.cookies?.access_token as string | null;
+        },
+      ]),
+
+      ignoreExpiration: false,
+
+      secretOrKey: configService.getOrThrow<string>('JWT_SECRET'),
+    });
+  }
+
+  validate(payload: JwtPayload) {
+    return {
+      id: payload.sub,
+      email: payload.email,
+    };
+  }
+}
