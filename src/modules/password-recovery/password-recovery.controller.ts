1255cdab04d68bb3e5dc50a9af35f829278055b3
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-08-26 14:40:11 -0300

    feat: adiciona endpoint de resete de senha

--- src/modules/password-recovery/password-recovery.controller.ts
+++ src/modules/password-recovery/password-recovery.controller.ts
@@ -3,6 +3,7 @@ import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { RequestPasswordRecoveryDto } from './dto/request-password-recovery.dto';
 import { PasswordRecoveryService } from './password-recovery.service';
 import { VerifyPasswordRecoveryDto } from './dto/verify-password-recovery.dto';
+import { ResetPasswordDto } from './dto/reset-password.dto';
 
 @Controller('auth/password-recovery')
 export class PasswordRecoveryController {
@@ -20,5 +21,11 @@ export class PasswordRecoveryController {
   @HttpCode(HttpStatus.OK)
   verifyRecoveryCode(@Body() verifyDto: VerifyPasswordRecoveryDto) {
     return this.passwordRecoveryService.verifyRecoveryCode(verifyDto);
+  }
+
+  @Post('reset')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async resetPassword(@Body() resetDto: ResetPasswordDto): Promise<void> {
+    await this.passwordRecoveryService.resetPassword(resetDto);
   }
 }
